c64780708d8b661612e4deda9e840d993aeef383
Fsepulvedap <fsepulveda.87@gmail.com>
2024-11-02 21:15:30 +0000

    proyecto terminado

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -4,15 +4,15 @@ const SecondsCounter = ({seconds}) => {
     return (
         <div className="container-fluid bg-dark">
             <div className="row container text-white p-5 mx-auto">
-                <div className="col text-center">
+                <div className="col custom-card text-center">
                 <i className="fa-regular fa-clock"></i>     
                 </div>
-                <div className="col card bg-dark text-center">{Math.floor(seconds / 100000) % 10}</div>
-                <div className="col text-center">{Math.floor(seconds / 10000) % 10}</div>
-                <div className="col text-center">{Math.floor(seconds / 1000) % 10}</div>
-                <div className="col text-center">{Math.floor(seconds / 100) % 10}</div>
-                <div className="col text-center">{Math.floor(seconds / 10) % 10}</div>
-                <div className="col text-center">{Math.floor(seconds / 1) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 100000) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 10000) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 1000) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 100) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 10) % 10}</div>
+                <div className="col custom-card text-center">{Math.floor(seconds / 1) % 10}</div>
             </div>
         </div>
     )
